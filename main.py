f583acc1ee94be18ce1b2b9773b2a9f314e3f4d0
dc-singh <dcs20095@gmail.com>
2026-05-21 08:33:26 +0530

    update on db connection

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from src.utils.db import Base, engine
 from src.tasks.router import task_routes
 from src.users.router import user_routes
+import uvicorn
+import os
 
 
 Base.metadata.create_all(engine)
@@ -10,7 +12,14 @@ app = FastAPI(title = "This is my Task management Applicaiton")
 app.include_router(task_routes)
 app.include_router(user_routes)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
 
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port
+    )
 
 
 
--- src/utils/db.py
+++ src/utils/db.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.utils.settings import Settings
+import os
+
+DATABASE_URL = os.getenv("DB_CONNECTION", Settings.DB_CONNECTION)
 
 Base = declarative_base()
 
-engine = create_engine(url=Settings.DB_CONNECTION)
+engine = create_engine(url=DATABASE_URL)
 
 Session = sessionmaker(bind=engine)
 
