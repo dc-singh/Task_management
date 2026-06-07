9247f3d59a794e5b427eed825fcada68730396f5
dc-singh <dcs20095@gmail.com>
2026-06-07 23:08:47 +0530

    host and port added in the main file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from src.utils.db import Base, engine
 from src.tasks.router import task_routes
 from src.users.router import user_routes
+import uvicorn
+import os
 
 
 Base.metadata.create_all(engine)
@@ -12,3 +14,11 @@ app.include_router(user_routes)
 
 
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port
+    )
